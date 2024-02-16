efb71a899e0ad3afb8d2fc877ef26fc5646f6567
AllanLoz4 <jafethsito.ab@gmail.com>
2024-02-16 13:57:24 -0500

    First Commit

---
+++ Average.cs
@@ -0,0 +1,30 @@
+﻿/*******************************************************************************  
+ * Kean University  
+ * Spring 2024
+ * Course: CPS*3300*01 - Software Development with Frameworks
+ * Author: Allan Lozano Bardales, CPS*3300, 01 
+ * Lab #2 Create and Test a Library App
+ *******************************************************************************/
+
+namespace MyAverageLib
+{
+    public class Average
+    {
+        public static double MyAverage(double a, double b)
+        {
+            double average2 = (a + b) / 2;//Calculate the average of the two user input
+
+            // Using composite String
+            Console.WriteLine("Provided values: {0:f2} and {1:f2}, the average is {2:f2}\n", a, b, average2);
+            return average2;
+        }
+        public static double MyAverage(double a, double b, double c)
+        {
+            double average3 = (a + b + c) / 3;//Calculate the average of the three user input
+
+            // Using composite String to display the message
+            Console.WriteLine("Provided values: {0:f2}, {1:f2}, and {2:f2}, the average is {3:f2}", a, b, c, average3);
+            return average3;
+        }
+    }
+}
